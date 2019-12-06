26a348283dd6d6d19d3da18ae2f62a1ce0444fd0
vaibagga <vaibagga@gmail.com>
2019-12-06 22:02:12 +0530

    for running text

--- main_text.py
+++ main_text.py
@@ -4,11 +4,8 @@ from scraper import WebScraper
 import time
 start_time = time.time()
 def main():
-    paragraph_url = sys.argv[1]
-    question_path = sys.argv[2]
-    paragraph_path = sys.argv[3]
-    ws = WebScraper(paragraph_url)
-    ws.saveBody(paragraph_path)
+    question_path = sys.argv[1]
+    paragraph_path = sys.argv[2]
     os.system("python Test_Batch.py --paragraph " + paragraph_path + " --question " + question_path + " --model pytorch_model.bin --config_file Results/bert_config.json")
     print("Time", time.time() - start_time)
 if __name__ == "__main__":
